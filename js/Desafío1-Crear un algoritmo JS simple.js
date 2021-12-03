6617a168591c13445fbccd7c2083f3ce67d6bcaa
Martin Sosa Felauto <martinsosafelauto@gmail.com>
2021-12-03 09:41:13 -0300

    Agregado Desafio 2

--- js/Desafío1-Crear un algoritmo JS simple.js
+++
@@ -1,5 +0,0 @@
-let userName = prompt(`Hola! Bienvenid@ a M-Academy!
-Por Favor ingrese su nombre:`);
-
-alert(`Bienvenid@ a M-Academy, ${userName}!
-Que disfrutes de tu experiencia en nuestro sitio!`);
